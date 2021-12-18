ddebaadd148ae2f488176659abc056264208c240
Marion Grolleau <mariongrolleau@neuf.fr>
2021-12-18 10:56:40 +0100

    Created index.js for connected a BDD

--- index.js
+++ index.js
@@ -1,15 +1,35 @@
 const express = require('express');
+const cors = require('cors');
+const { backPort, db } = require('./config');
 
 const app = express();
 
-app.get('/characters', async (req, res) => {
-  res.status(404).send('Route not found! ');
+app.use(cors());
+app.use(express.json());
+
+app.get('/cards', async (req, res) => {
+  try {
+    const [rows] = await db.query('SELECT * FROM cards');
+
+    res.status(200).json(rows);
+  } catch (err) {
+    res.status(400).send(err);
+  }
+});
+
+app.get('/toto', async (req, res) => {
+  try {
+    const [rows] = await db.query('SELECT id FROM cards');
+    res.status(200).send(rows);
+  } catch (err) {
+    res.status(400).send(err);
+  }
 });
 
 app.use('/', (req, res) => {
-  res.status(404).send('Route not found! ');
+  res.status(404).send('Route not found ! ');
 });
 
-app.listen(5050, () => {
-  console.log('Terra Battle API now available on http://localhost:5050 !');
+app.listen(backPort, () => {
+  console.log(`API now available on http://localhost:${backPort} !`);
 });
