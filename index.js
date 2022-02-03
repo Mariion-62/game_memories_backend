a3a3b5c1207e5e4af9d92e53c5e45f5e458a043a
Marion Grolleau <mariongrolleau@neuf.fr>
2022-02-03 17:17:29 +0100

    Created route get, post, get:id, put and delete

--- index.js
+++ index.js
@@ -17,6 +17,60 @@ app.get('/cards', async (req, res) => {
   }
 });
 
+app.get('/cards/:id', async (req, res) => {
+  const { id } = req.params;
+  try {
+    const [card] = await db.query(
+      `SELECT id, picture, name, type
+      FROM cards
+      WHERE id=?
+      `,
+      [id]
+    );
+    if (card.length) res.json(card);
+    else throw new Error('Pas de carte!');
+  } catch (err) {
+    console.warn(err);
+    res.status(404).send('ERROR');
+  }
+});
+
+app.post('/cards', async (req, res) => {
+  const { picture, name, type } = await req.body;
+  await db.query(
+    `INSERT INTO cards (picture, name, type)
+      VALUES (?,?,?)`,
+    [picture, name, type]
+  );
+  res.status(204).send('Tu as ajouté une nouvelle carte !');
+});
+
+app.put('/cards/:id', async (req, res) => {
+  const { picture, name, type } = req.body;
+  const { id } = req.params;
+  await db.query(
+    `
+    UPDATE cards
+    SET picture=?, name=?, type=?
+    WHERE id=?
+    `,
+    [picture, name, type, id]
+  );
+  res.status(204).send('Tu as mis à jour cette carte');
+});
+
+app.delete('/cards/:id', async (req, res) => {
+  const { id } = req.params;
+  await db.query(
+    `
+  DELETE FROM cards
+  WHERE id=?
+  `,
+    [id]
+  );
+  res.status(204).send('Tu as supprimé une carte');
+});
+
 app.use('/', (req, res) => {
   res.status(404).send('Route not found ! ');
 });
